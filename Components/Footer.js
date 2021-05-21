1af24255cc298a4eda2b58e7c2f1308ac32fd7fd
Almas Khan <almaskhanwazir@gmail.com>
2021-05-21 20:21:22 +0500

    more changes

---
+++ Components/Footer.js
@@ -0,0 +1,88 @@
+import { useState } from "react";
+import Router from "next/router";
+import Link from "next/link";
+const Footer = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className="footer-Section">
+      <div className="col-md-10 offset-md-1 ">
+        <div className="row">
+          <div class="col-md-4">
+            <p>connect with us</p>
+            <hr />
+            <ul>
+              <Link href="/">
+                <li>
+                  <img
+                    className=""
+                    src="/static/svg/fbIcon2.svg"
+                    alt=""
+                  />
+                </li>
+              </Link>
+              <Link href="/">
+                <li>
+                <img
+                    className=""
+                    src="/static/svg/instaTransparent.svg"
+                    alt=""
+                  />
+                </li>
+              </Link>
+              <Link href="/">
+                <li>
+                <img
+                    className=""
+                    src="/static/svg/playIcon.svg"
+                    alt=""
+                  />
+                </li>
+              </Link>
+              <Link href="/">
+                <li>
+                <img
+                    className=""
+                    src="/static/svg/linkedin.svg"
+                    alt=""
+                  />
+                </li>
+              </Link>
+              <Link href="/">
+                <li>
+                <img
+                    className=""
+                    src="/static/svg/twitter.svg"
+                    alt=""
+                  />
+                </li>
+              </Link>
+            </ul>
+          </div>
+          <div class="col-md-4">copyright &#169 2021. iPurvey;</div>
+          <div class="col-md-4">
+          <ul>
+                  <Link href="/">
+                    <li>
+                      <a className="nav-link scrollto">Privacy policy</a>
+                    </li>
+                  </Link>
+                 <p>/</p>
+                  <Link href="/">
+                    <li>
+                      <a className="nav-link scrollto">Term of use</a>
+                    </li>
+                  </Link>
+                </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
--- Components/HeroSection.js
+++ Components/HeroSection.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Container, Row, Col } from "reactstrap";
+import Link from "next/link";
 import SubscribeForm from "./SubscribeForm";
+import Footer from "./Footer";
 const HeroSection = (props) => {
   return (
     <>
@@ -42,7 +44,7 @@ const HeroSection = (props) => {
         <img className="herobtm" src="/static/svg/heroBottom.svg" alt="" />
       </section>
       <div class="row station">
-         <img className="stationImg" src="/static/svg/station.svg" alt="" />
+        <img className="stationImg" src="/static/svg/station.svg" alt="" />
         <img className="trackImg" src="/static/svg/train-track.svg" alt="" />
         <img className="trainImg" src="/static/svg/train.svg" alt="" />
       </div>
@@ -71,27 +73,115 @@ const HeroSection = (props) => {
                 enim ad minim veniam, quis nostrud exercitation ullamco laboris
               </p>
               <div class="col-md-6 offset-md-3 section4">
-                <h2>
-                Comming Soon
-                </h2>
-               <img
-                className="trainImg"
-                src="/static/png/storeIcons3x.png"
-                alt=""
-              />
-                
-                
-                </div>
+                <h2>Comming Soon</h2>
+                <img
+                  className="trainImg"
+                  src="/static/png/storeIcons3x.png"
+                  alt=""
+                />
+              </div>
             </div>
             <div class="col-md-4">
               <img
-                className="trainImg"
+                className=""
                 src="/static/png/mobileapp.png"
                 alt=""
               />
             </div>
           </div>
         </div>
+        <div class="section5">
+          <div className="col-md-10 offset-md-1 ">
+            <div class="row">
+              <div class="col-md-8">
+                <img
+                  className=""
+                  src="/static/svg/ipurvey-logo2.svg"
+                  alt=""
+                />
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do
+                </p>
+                <p>
+                  Utenim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris eiusmod tempor incididunt ut labore
+                </p>
+                <p>
+                  enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris et dolore magna aliqua.
+                </p>
+                <ul>
+                  <Link href="/">
+                    <li>
+                      <a className="nav-link scrollto">Home</a>
+                    </li>
+                  </Link>
+                  <Link href="/">
+                    <li>
+                      <a className="nav-link scrollto">About us</a>
+                    </li>
+                  </Link>
+                  <Link href="/">
+                    <li>
+                      <a className="nav-link scrollto">Contact us</a>
+                    </li>
+                  </Link>
+                  <Link href="/">
+                    <li>
+                      <a className="nav-link scrollto">Blogs</a>
+                    </li>
+                  </Link>
+                </ul>
+              </div>
+              <div class="col-md-4">
+                <h1>Get in touch!</h1>
+                <form class="email-form">
+                  <div class="row gy-4">
+                    <div class="col-md-12">
+                      <input
+                        type="text"
+                        name="name"
+                        class="form-control"
+                        placeholder="Full Name"
+                        required
+                      />
+                    </div>
+
+                    <div class="col-md-12 ">
+                      <input
+                        type="email"
+                        class="form-control"
+                        name="email"
+                        placeholder="Email Address"
+                        required
+                      />
+                    </div>
+
+                    <div class="col-md-12">
+                      <textarea
+                        class="form-control"
+                        name="message"
+                        rows="6"
+                        placeholder="Message"
+                        required
+                      ></textarea>
+                    </div>
+
+                    <div class="col-md-12 text-center">
+                      {/* <div class="loading">Loading</div>
+                  <div class="error-message"></div>
+                  <div class="sent-message">Your message has been sent. Thank you!</div> */}
+
+                      <button type="submit">Send </button>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+        <Footer/>
       </div>
     </>
   );
